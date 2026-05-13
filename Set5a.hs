9446f44f922ea203fe4818074f5e9e143582bc3d
Amit Srivastava <xkimdr@gmail.com>
2026-05-13 06:27:54 +0530

    feat: upload - completed part 1 exercises

---
+++ Set5a.hs
@@ -0,0 +1,357 @@
+-- Exercise set 5a
+--
+-- \* defining algebraic datatypes
+-- \* recursive datatypes
+
+module Set5a where
+
+import Mooc.Todo
+
+------------------------------------------------------------------------------
+-- Ex 1: Define the type Vehicle that has four constructors: Bike,
+-- Bus, Tram and Train.
+--
+-- The constructors don't need any fields.
+
+data Vehicle = Bike | Bus | Tram | Train
+
+------------------------------------------------------------------------------
+-- Ex 2: Define the type BusTicket that can represent values like these:
+--  - SingleTicket
+--  - MonthlyTicket "January"
+--  - MonthlyTicket "December"
+
+data BusTicket = MonthlyTicket String | SingleTicket
+
+------------------------------------------------------------------------------
+-- Ex 3: Here's the definition for a datatype ShoppingEntry that
+-- represents an entry in a shopping basket. It has an item name (a
+-- String), an item price (a Double) and a count (an Int). You'll also
+-- find two examples of ShoppingEntry values.
+--
+-- Implement the functions totalPrice and buyOneMore below.
+
+data ShoppingEntry = MkShoppingEntry String Double Int
+  deriving (Show)
+
+threeApples :: ShoppingEntry
+threeApples = MkShoppingEntry "Apple" 0.5 3
+
+twoBananas :: ShoppingEntry
+twoBananas = MkShoppingEntry "Banana" 1.1 2
+
+-- totalPrice should return the total price for an entry
+--
+-- Hint: you'll probably need fromIntegral to convert the Int into a
+-- Double
+--
+-- Examples:
+--   totalPrice threeApples  ==> 1.5
+--   totalPrice twoBananas   ==> 2.2
+
+totalPrice :: ShoppingEntry -> Double
+totalPrice (MkShoppingEntry _ price count) = price * fromIntegral count
+
+-- buyOneMore should increment the count in an entry by one
+--
+-- Example:
+--   buyOneMore twoBananas    ==> MkShoppingEntry "Banana" 1.1 3
+
+buyOneMore :: ShoppingEntry -> ShoppingEntry
+buyOneMore (MkShoppingEntry name price count) = MkShoppingEntry name price (count + 1)
+
+------------------------------------------------------------------------------
+-- Ex 4: define a datatype Person, which should contain the age (an
+-- Int) and the name (a String) of a person.
+--
+-- Also define a Person value fred, and the functions getAge, getName,
+-- setAge and setName (see below).
+
+data Person = MkPerson {name :: String, age :: Int}
+  deriving (Show)
+
+-- fred is a person whose name is Fred and age is 90
+fred :: Person
+fred = MkPerson {name = "Fred", age = 90}
+
+-- getName returns the name of the person
+getName :: Person -> String
+getName = name
+
+-- getAge returns the age of the person
+getAge :: Person -> Int
+getAge = age
+
+-- setName takes a person and returns a new person with the name changed
+setName :: String -> Person -> Person
+setName name p = p {name = name}
+
+-- setAge does likewise for age
+setAge :: Int -> Person -> Person
+setAge age p = p {age = age}
+
+------------------------------------------------------------------------------
+-- Ex 5: define a datatype Position which contains two Int values, x
+-- and y. Also define the functions below for operating on a Position.
+--
+-- Examples:
+--   getY (up (up origin))    ==> 2
+--   getX (up (right origin)) ==> 1
+
+data Position = MkPosition {x :: Int, y :: Int}
+
+-- origin is a Position value with x and y set to 0
+origin :: Position
+origin = MkPosition {x = 0, y = 0}
+
+-- getX returns the x of a Position
+getX :: Position -> Int
+getX = x
+
+-- getY returns the y of a position
+getY :: Position -> Int
+getY = y
+
+-- up increases the y value of a position by one
+up :: Position -> Position
+up p = p {y = y p + 1}
+
+-- right increases the x value of a position by one
+right :: Position -> Position
+right p = p {x = x p + 1}
+
+------------------------------------------------------------------------------
+-- Ex 6: Here's a datatype that represents a student. A student can
+-- either be a freshman, a nth year student, or graduated.
+
+data Student = Freshman | NthYear Int | Graduated
+  deriving (Show, Eq)
+
+-- Implement the function study, which changes a Freshman into a 1st
+-- year student, a 1st year student into a 2nd year student, and so
+-- on. A 7th year student gets changed to a graduated student. A
+-- graduated student stays graduated even if he studies.
+
+study :: Student -> Student
+study stud = case stud of
+  Freshman -> NthYear 1
+  NthYear x
+    | x < 7 -> NthYear (x + 1)
+    | otherwise -> Graduated
+  Graduated -> Graduated
+
+------------------------------------------------------------------------------
+-- Ex 7: define a datatype UpDown that represents a counter that can
+-- either be in increasing or decreasing mode. Also implement the
+-- functions zero, toggle, tick and get below.
+--
+-- NB! Define _two_ constructors for your datatype (feel free to name the
+-- constructors however you want)
+--
+-- Examples:
+--
+-- get (tick zero)
+--   ==> 1
+-- get (tick (tick zero))
+--   ==> 2
+-- get (tick (tick (toggle (tick zero))))
+--   ==> -1
+
+data UpDown = UpDownInc Int | UpDownDec Int
+
+-- zero is an increasing counter with value 0
+zero :: UpDown
+zero = UpDownInc 0
+
+-- get returns the counter value
+get :: UpDown -> Int
+get (UpDownInc x) = x
+get (UpDownDec x) = x
+
+-- tick increases an increasing counter by one or decreases a
+-- decreasing counter by one
+tick :: UpDown -> UpDown
+tick (UpDownInc x) = UpDownInc (x + 1)
+tick (UpDownDec x) = UpDownDec (x - 1)
+
+-- toggle changes an increasing counter into a decreasing counter and
+-- vice versa
+toggle :: UpDown -> UpDown
+toggle (UpDownDec x) = UpDownInc x
+toggle (UpDownInc x) = UpDownDec x
+
+------------------------------------------------------------------------------
+-- Ex 8: you'll find a Color datatype below. It has the three basic
+-- colours Red, Green and Blue, and two color transformations, Mix and
+-- Invert.
+--
+-- Mix means the average of the two colors in each rgb channel.
+--
+-- Invert means subtracting all rgb values from 1.
+--
+-- Implement the function rgb :: Color -> [Double] that returns a list
+-- of length three that represents the rgb value of the given color.
+--
+-- Examples:
+--
+-- rgb Red   ==> [1,0,0]
+-- rgb Green ==> [0,1,0]
+-- rgb Blue  ==> [0,0,1]
+--
+-- rgb (Mix Red Green)                    ==> [0.5,0.5,0]
+-- rgb (Mix Red (Mix Red Green))          ==> [0.75,0.25,0]
+-- rgb (Invert Red)                       ==> [0,1,1]
+-- rgb (Invert (Mix Red (Mix Red Green))) ==> [0.25,0.75,1]
+-- rgb (Mix (Invert Red) (Invert Green))  ==> [0.5,0.5,1]
+
+data Color = Red | Green | Blue | Mix Color Color | Invert Color
+  deriving (Show)
+
+rgb :: Color -> [Double]
+rgb Red = [1, 0, 0]
+rgb Green = [0, 1, 0]
+rgb Blue = [0, 0, 1]
+rgb (Mix x y) = map (/ 2) (zipWith (+) (rgb x) (rgb y))
+rgb (Invert x) = map (1 -) (rgb x)
+
+------------------------------------------------------------------------------
+-- Ex 9: define a parameterized datatype OneOrTwo that contains one or
+-- two values of the given type. The constructors should be called One and Two.
+--
+-- Examples:
+--   One True         ::  OneOrTwo Bool
+--   Two "cat" "dog"  ::  OneOrTwo String
+
+data OneOrTwo a = One a | Two a a
+
+------------------------------------------------------------------------------
+-- Ex 10: define a recursive datatype KeyVals for storing a set of
+-- key-value pairs. There should be two constructors: Empty and Pair.
+--
+-- Empty represents an empty collection. It should have no fields.
+--
+-- Pair should have three fields, one for the key, one for the value,
+-- and one for the rest of the collection (of type KeyVals)
+--
+-- The KeyVals datatype is parameterized by the key type k and
+-- the value type v.
+--
+-- For example:
+--
+--  Pair "cat" True (Pair "dog" False Empty)  ::  KeyVals String Bool
+--
+-- Also define the functions toList and fromList that convert between
+-- KeyVals and lists of pairs.
+
+data KeyVals k v = Pair k v (KeyVals k v) | Empty
+  deriving (Show)
+
+toList :: KeyVals k v -> [(k, v)]
+toList Empty = []
+toList (Pair k v x) = (k, v) : toList x
+
+fromList :: [(k, v)] -> KeyVals k v
+fromList [] = Empty
+fromList ((k, v) : xs) = Pair k v (fromList xs)
+
+------------------------------------------------------------------------------
+-- Ex 11: The data type Nat is the so called Peano
+-- representation for natural numbers. Define functions fromNat and
+-- toNat that convert natural numbers to Ints and vice versa.
+--
+-- Examples:
+--   fromNat (PlusOne (PlusOne (PlusOne Zero)))  ==>  3
+--   toNat 3    ==> Just (PlusOne (PlusOne (PlusOne Zero)))
+--   toNat (-3) ==> Nothing
+--
+
+data Nat = Zero | PlusOne Nat
+  deriving (Show, Eq)
+
+fromNat :: Nat -> Int
+fromNat Zero = 0
+fromNat (PlusOne x) = 1 + fromNat x
+
+toNat :: Int -> Maybe Nat
+toNat z
+  | z < 0 = Nothing
+  | otherwise = Just (toNat' z)
+  where
+    toNat' 0 = Zero
+    toNat' z = PlusOne (toNat' (z - 1))
+
+------------------------------------------------------------------------------
+-- Ex 12: While pleasingly simple in its definition, the Nat datatype is not
+-- very efficient computationally. Instead of the unary Peano natural numbers,
+-- computers use binary numbers.
+--
+-- Binary numbers are like decimal numbers, except that binary numbers have
+-- only two digits (called bits), 0 and 1. The table below gives some
+-- examples:
+--
+--   decimal | binary
+--   --------+-------
+--         0 |      0
+--         1 |      1
+--         2 |     10
+--         7 |    111
+--        44 | 101100
+--
+-- For allowing arbitrarily long binary numbers, our representation, the
+-- datatype Bin, includes a special End constructor for denoting the end of
+-- the binary number. In order to make computation with Bin easier, the bits
+-- are represented in increasing order by significance (i.e. "backwards").
+-- Consider the Bin numbers O (I (I End)), representing 110 in binary or
+-- 6 in decimal, and I (I (O End)) that represents 011 in binary or 3 in
+-- decimal. The most significant (last) bit, the bit I, of O (I (I End)) is
+-- greater than the bit O, which is the most significant bit of I (I (O End)).
+-- Therefore, O (I (I End)) is greater than I (I (O End)).
+--
+-- Your task is to write functions prettyPrint, fromBin, and toBin that
+-- convert Bin to human-readable string, Bin to Int, and Int to Bin
+-- respectively.
+--
+-- Examples:
+--   prettyPrint End                     ==> ""
+--   prettyPrint (O End)                 ==> "0"
+--   prettyPrint (I End)                 ==> "1"
+--   prettyPrint (O (O (I (O (I End))))) ==> "10100"
+--   map fromBin [O End, I End, O (I End), I (I End), O (O (I End)),
+--                  I (O (I End))]
+--     ==> [0, 1, 2, 3, 4, 5]
+--   fromBin (I (I (O (O (I (O (I (O End)))))))) ==> 83
+--   fromBin (I (I (O (O (I (O (I End)))))))     ==> 83
+--   map toBin [0..5] ==>
+--     [O End,I End,O (I End),I (I End),O (O (I End)),I (O (I End))]
+--   toBin 57 ==> I (O (O (I (I (I End)))))
+--
+-- Challenge: Can you implement toBin by directly converting its input into a
+-- sequence of bits instead of repeatedly applying inc?
+--
+data Bin = End | O Bin | I Bin
+  deriving (Show, Eq)
+
+-- This function increments a binary number by one.
+inc :: Bin -> Bin
+inc End = I End
+inc (O b) = I b
+inc (I b) = O (inc b)
+
+prettyPrint :: Bin -> String
+prettyPrint End = ""
+prettyPrint (I b) = prettyPrint b ++ "1"
+prettyPrint (O b) = prettyPrint b ++ "0"
+
+fromBin :: Bin -> Int
+fromBin b = fromBin' b 0 0
+  where
+    fromBin' End _ x = x
+    fromBin' (I b) n x = fromBin' b (n + 1) (x + 2 ^ n)
+    fromBin' (O b) n x = fromBin' b (n + 1) x
+
+toBin :: Int -> Bin
+toBin x
+  | x == 0 = O End
+  | x == 1 = I End
+  | even x = O (toBin (div x 2))
+  | otherwise = I (toBin (div x 2))
---
+++ Set5b.hs
@@ -0,0 +1,235 @@
+-- Exercise set 5b: playing with binary trees
+{-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
+
+{-# HLINT ignore "Use isNothing" #-}
+
+module Set5b where
+
+import Mooc.Todo
+
+-- The next exercises use the binary tree type defined like this:
+
+data Tree a = Empty | Node a (Tree a) (Tree a)
+  deriving (Show, Eq)
+
+------------------------------------------------------------------------------
+-- Ex 1: implement the function valAtRoot which returns the value at
+-- the root (top-most node) of the tree. The return value is Maybe a
+-- because the tree might be empty (i.e. just a Empty)
+
+valAtRoot :: Tree a -> Maybe a
+valAtRoot Empty = Nothing
+valAtRoot (Node a _ _) = Just a
+
+------------------------------------------------------------------------------
+-- Ex 2: compute the size of a tree, that is, the number of Node
+-- constructors in it
+--
+-- Examples:
+--   treeSize (Node 3 (Node 7 Empty Empty) Empty)  ==>  2
+--   treeSize (Node 3 (Node 7 Empty Empty) (Node 1 Empty Empty))  ==>  3
+
+treeSize :: Tree a -> Int
+treeSize Empty = 0
+treeSize (Node a t1 t2) = 1 + treeSize t1 + treeSize t2
+
+------------------------------------------------------------------------------
+-- Ex 3: get the largest value in a tree of positive Ints. The
+-- largest value of an empty tree should be 0.
+--
+-- Examples:
+--   treeMax Empty  ==>  0
+--   treeMax (Node 3 (Node 5 Empty Empty) (Node 4 Empty Empty))  ==>  5
+
+treeMax :: Tree Int -> Int
+treeMax Empty = 0
+treeMax (Node a t1 t2) = max a (max (treeMax t1) (treeMax t2))
+
+------------------------------------------------------------------------------
+-- Ex 4: implement a function that checks if all tree values satisfy a
+-- condition.
+--
+-- Examples:
+--   allValues (>0) Empty  ==>  True
+--   allValues (>0) (Node 1 Empty (Node 2 Empty Empty))  ==>  True
+--   allValues (>0) (Node 1 Empty (Node 0 Empty Empty))  ==>  False
+
+allValues :: (a -> Bool) -> Tree a -> Bool
+allValues condition Empty = True
+allValues condition (Node a t1 t2) = condition a && allValues condition t1 && allValues condition t2
+
+------------------------------------------------------------------------------
+-- Ex 5: implement map for trees.
+--
+-- Examples:
+--
+-- mapTree (+1) Empty  ==>  Empty
+-- mapTree (+2) (Node 0 (Node 1 Empty Empty) (Node 2 Empty Empty))
+--   ==> (Node 2 (Node 3 Empty Empty) (Node 4 Empty Empty))
+
+mapTree :: (a -> b) -> Tree a -> Tree b
+mapTree f Empty = Empty
+mapTree f (Node a t1 t2) = Node (f a) (mapTree f t1) (mapTree f t2)
+
+------------------------------------------------------------------------------
+-- Ex 6: given a value and a tree, build a new tree that is the same,
+-- except all nodes that contain the value have been removed. Also
+-- remove the subnodes of the removed nodes.
+--
+-- Examples:
+--
+--     1          1
+--    / \   ==>    \
+--   2   0          0
+--
+--  cull 2 (Node 1 (Node 2 Empty Empty)
+--                 (Node 0 Empty Empty))
+--     ==> (Node 1 Empty
+--                 (Node 0 Empty Empty))
+--
+--      1           1
+--     / \           \
+--    2   0   ==>     0
+--   / \
+--  3   4
+--
+--  cull 2 (Node 1 (Node 2 (Node 3 Empty Empty)
+--                         (Node 4 Empty Empty))
+--                 (Node 0 Empty Empty))
+--     ==> (Node 1 Empty
+--                 (Node 0 Empty Empty)
+--
+--    1              1
+--   / \              \
+--  0   3    ==>       3
+--   \   \
+--    2   0
+--
+--  cull 0 (Node 1 (Node 0 Empty
+--                         (Node 2 Empty Empty))
+--                 (Node 3 Empty
+--                         (Node 0 Empty Empty)))
+--     ==> (Node 1 Empty
+--                 (Node 3 Empty Empty))
+
+cull :: Eq a => a -> Tree a -> Tree a
+cull _ Empty = Empty
+cull val (Node a t1 t2)
+  | a == val = Empty
+  | valAtRoot t1 == Just val = Node a Empty (cull val t2)
+  | valAtRoot t2 == Just val = Node a (cull val t1) Empty
+  | otherwise = Node a (cull val t1) (cull val t2)
+
+------------------------------------------------------------------------------
+-- Ex 7: check if a tree is ordered. A tree is ordered if:
+--  * all values to the left of the root are smaller than the root value
+--  * all of the values to the right of the root are larger than the root value
+--  * and the left and right subtrees are ordered.
+--
+-- Hint: allValues will help you here!
+--
+-- Examples:
+--         1
+--        / \   is ordered:
+--       0   2
+--   isOrdered (Node 1 (Node 0 Empty Empty)
+--                     (Node 2 Empty Empty))   ==>   True
+--
+--         1
+--        / \   is not ordered:
+--       2   3
+--   isOrdered (Node 1 (Node 2 Empty Empty)
+--                     (Node 3 Empty Empty))   ==>   False
+--
+--           2
+--         /   \
+--        1     3   is not ordered:
+--         \
+--          0
+--   isOrdered (Node 2 (Node 1 Empty
+--                             (Node 0 Empty Empty))
+--                     (Node 3 Empty Empty))   ==>   False
+--
+--           2
+--         /   \
+--        0     3   is ordered:
+--         \
+--          1
+--   isOrdered (Node 2 (Node 0 Empty
+--                             (Node 1 Empty Empty))
+--                     (Node 3 Empty Empty))   ==>   True
+
+isOrdered :: Ord a => Tree a -> Bool
+isOrdered Empty = True
+isOrdered (Node a Empty Empty) = True
+isOrdered (Node a t1 Empty) = valAtRoot t1 < Just a && isOrdered t1
+isOrdered (Node a Empty t2) = valAtRoot t2 > Just a && isOrdered t2
+isOrdered (Node a t1 t2) = valAtRoot t1 < Just a && valAtRoot t2 > Just a && isOrdered t1 && isOrdered t2
+
+------------------------------------------------------------------------------
+-- Ex 8: a path in a tree can be represented as a list of steps that
+-- go either left or right.
+
+data Step = StepL | StepR
+  deriving (Show, Eq)
+
+-- Define a function walk that takes a tree and a list of steps, and
+-- returns the value at that point. Return Nothing if you fall of the
+-- tree (i.e. hit a Empty).
+--
+-- Examples:
+--   walk [] (Node 1 (Node 2 Empty Empty) Empty)       ==>  Just 1
+--   walk [StepL] (Node 1 (Node 2 Empty Empty) Empty)  ==>  Just 2
+--   walk [StepL,StepL] (Node 1 (Node 2 Empty Empty) Empty)  ==>  Nothing
+
+walk :: [Step] -> Tree a -> Maybe a
+walk _ Empty = Nothing
+walk [] (Node a _ _) = Just a
+walk (StepL : xs) (Node _ t _) = walk xs t
+walk (StepR : xs) (Node _ _ t) = walk xs t
+
+------------------------------------------------------------------------------
+-- Ex 9: given a tree, a path and a value, set the value at the end of
+-- the path to the given value. Since Haskell datastructures are
+-- immutable, you'll need to build a new tree.
+--
+-- If the path falls off the tree, do nothing.
+--
+-- Examples:
+--   set [] 1 (Node 0 Empty Empty)  ==>  (Node 1 Empty Empty)
+--   set [StepL,StepL] 1 (Node 0 (Node 0 (Node 0 Empty Empty)
+--                                       (Node 0 Empty Empty))
+--                               (Node 0 Empty Empty))
+--                  ==>  (Node 0 (Node 0 (Node 1 Empty Empty)
+--                                       (Node 0 Empty Empty))
+--                               (Node 0 Empty Empty))
+--
+--   set [StepL,StepR] 1 (Node 0 Empty Empty)  ==>  (Node 0 Empty Empty)
+
+set :: [Step] -> a -> Tree a -> Tree a
+set [] val (Node _ t1 t2) = Node val t1 t2
+set (StepL : xs) val (Node a t1 t2) = Node a (set xs val t1) t2
+set (StepR : xs) val (Node a t1 t2) = Node a t1 (set xs val t2)
+set _ _ Empty = Empty
+
+------------------------------------------------------------------------------
+-- Ex 10: given a value and a tree, return a path that goes from the
+-- root to the value. If the value doesn't exist in the tree, return Nothing.
+--
+-- You may assume the value occurs in the tree at most once.
+--
+-- Examples:
+--   search 1 (Node 2 (Node 1 Empty Empty) (Node 3 Empty Empty))  ==>  Just [StepL]
+--   search 1 (Node 2 (Node 4 Empty Empty) (Node 3 Empty Empty))  ==>  Nothing
+--   search 1 (Node 2 (Node 3 (Node 4 Empty Empty)
+--                            (Node 1 Empty Empty))
+--                    (Node 5 Empty Empty))                     ==>  Just [StepL,StepR]
+
+search :: Eq a => a -> Tree a -> Maybe [Step]
+search val tree = search' val tree []
+  where
+    search' _ Empty _ = Nothing
+    search' val (Node x t1 t2) xs
+      | val == x = Just xs
+      | search' val t1 (xs ++ [StepL]) == Nothing = search' val t2 (xs ++ [StepR])
+      | otherwise = search' val t1 (xs ++ [StepL])
